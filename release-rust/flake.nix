7416b06880c93229379b3a38b75aacce5414a88d
jurraca <julienu@pm.me>
2023-12-02 16:53:35 +0100

    use the right nixpkgs URL

--- release-rust/flake.nix
+++ release-rust/flake.nix
@@ -2,7 +2,7 @@
   description = "(a description of your package goes here)";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixpkgs-23.11;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-23.11;
     flake-utils.url = github:numtide/flake-utils;
     rust-overlay.url = github:oxalica/rust-overlay;
   };
