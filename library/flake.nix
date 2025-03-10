a63c79824d927912c7e6705d4035bac0ddc7a551
jurraca <julienu@pm.me>
2025-03-10 14:55:05 +0000

    library: refactor flake, remove flake-utils

--- library/flake.nix
+++ library/flake.nix
@@ -3,40 +3,64 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-24.11;
-    flake-utils.url = github:numtide/flake-utils;
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    # build for each default system of flake-utils: ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"]
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system ; };
-        # declare OTP version via `erlang` attribute. If not, defaults to the latest on this channel.
-        beamPackages = pkgs.beam.packagesWith pkgs.beam.interpreters.erlang_27;
-        # declare the Elixir version you want to use. If not, defaults to the latest on this channel.
-        elixir = beamPackages.elixir_1_18;
-        # import a development shell we'll declare in `shell.nix`.
-        devShell = import ./shell.nix { inherit pkgs beamPackages; };
+  outputs = { self, nixpkgs }: let
+    overlay = prev: final: rec {
+      beamPackages = prev.beam.packagesWith prev.beam.interpreters.erlang_27;
+      elixir = beamPackages.elixir_1_17;
+      erlang = prev.erlang_27;
+      hex = beamPackages.hex;
+      final.mix2nix = prev.mix2nix.overrideAttrs {
+        nativeBuildInputs = [ final.elixir ];
+        buildInputs = [ final.erlang ];
+      };
+    };
 
-        my-elixir-app = let
-            lib = pkgs.lib;
-            # FIXME: import the Mix deps into Nix by running `mix2nix > deps.nix`
-            # mixNixDeps = import ./deps.nix { inherit lib beamPackages; };
-          in beamPackages.buildMix {
-            name = "my-elixir-app";
-            # Elixir app source path
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+
+    nixpkgsFor = system:
+      import nixpkgs {
+        inherit system;
+        overlays = [overlay];
+      };
+    in {
+    packages = forAllSystems(system: let
+      pkgs = nixpkgsFor system;
+      # FIXME: import the Mix deps into Nix by running `mix2nix > deps.nix` from a dev shell
+      mixNixDeps = import ./deps.nix {
+        lib = pkgs.lib;
+        beamPackages = pkgs.beamPackages;
+      };
+      in rec {
+        default = pkgs.beamPackages.buildMix {
+            name = "my-elixir-lib";
+            # Elixir lib source path
             src = ./.;
             version = "0.1.0";
-            # Add inputs to the build if you need to
-            buildInputs = [ elixir ];
-            #  FIXME: declare the nix paths of the mix dependencies from the mixNixDeps attr above.
-            # beamDeps = builtins.attrValues mixNixDeps;
+            beamDeps = builtins.attrValues mixNixDeps;
+	    # Add inputs to the build if you need to
+            buildInputs = [ pkgs.elixir ];
+	    buildPhase = ''
+              runHook preBuild
+              export HEX_HOME=".nix-hex";
+              export MIX_HOME=".nix-mix";
+              mix compile --no-deps-check
+              runHook postBuild
+	    '';
           };
-      in
-      {
-        devShells.default = devShell;
-        packages.default = my-elixir-app;
-      }
-    );
+      });
+
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor system;
+    in {
+      default = pkgs.callPackage ./shell.nix {};
+    });
+  };
 }
 
--- library/shell.nix
+++ library/shell.nix
@@ -1,44 +1,34 @@
-{ pkgs, beamPackages }:
-
-with pkgs; let
-
-  opts =
-    lib.optional stdenv.isLinux inotify-tools ++
-    lib.optionals stdenv.isDarwin (with darwin.apple_sdk.frameworks; [
-      CoreServices
-      Foundation
-    ]);
-
-  buildInputs =
-    let
-      inherit beamPackages;
-      elixir = beamPackages.elixir;
-      hex = beamPackages.hex;
-    in
-    [
-      elixir
-      mix2nix
-      hex
-      rebar3
-    ] ++ opts;
-
-  shellHook = ''
-    # Set up `mix` to save dependencies to the local directory
-    mkdir -p .nix-mix
-    mkdir -p .nix-hex
-    export MIX_HOME=$PWD/.nix-mix
-    export HEX_HOME=$PWD/.nix-hex
-    export PATH=$MIX_HOME/bin:$PATH
-    export PATH=$HEX_HOME/bin:$PATH
-
-    # Beam-specific
-    export LANG=en_US.UTF-8
-    export ERL_AFLAGS="-kernel shell_history enabled"
-  '';
-
+{
+  pkgs,
+  lib
+}: let
+  # define packages to install
+  basePackages = with pkgs; [
+    elixir
+    hex
+    mix2nix
+  ];
 in
-mkShell {
-  inherit
-    buildInputs
-    shellHook;
-}
+  pkgs.mkShell {
+    # Add basePackages + optional system packages per system
+    buildInputs = with pkgs;
+      basePackages
+      ++ lib.optionals stdenv.isLinux [inotify-tools]
+      ++ lib.optionals stdenv.isDarwin
+      (with darwin.apple_sdk.frameworks; [CoreFoundation CoreServices]);
+
+    # define shell startup command
+    shellHook = ''
+      # Set up `mix` to save dependencies to the local directory
+      mkdir -p .nix-mix
+      mkdir -p .nix-hex
+      export MIX_HOME=$PWD/.nix-mix
+      export HEX_HOME=$PWD/.nix-hex
+      export PATH=$MIX_HOME/bin:$PATH
+      export PATH=$HEX_HOME/bin:$PATH
+
+      # Beam-specific
+      export LANG=en_US.UTF-8
+      export ERL_AFLAGS="-kernel shell_history enabled"
+    '';
+  }
