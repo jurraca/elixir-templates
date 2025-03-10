13342874e50da4d2168f0c9f9df156b96c5717c9
jurraca <julienu@pm.me>
2025-03-10 15:16:00 +0000

    release flake: refactor, remove-flake-utils

--- release/flake.nix
+++ release/flake.nix
@@ -3,44 +3,61 @@
 
   inputs = {
     nixpkgs.url = github:NixOS/nixpkgs/nixos-24.11;
-    flake-utils.url = github:numtide/flake-utils;
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    # build for each default system of flake-utils: ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"]
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        # Declare pkgs for the specific target system we're building for.
-        pkgs = import nixpkgs { inherit system ; };
-        # Declare BEAM version we want to use. If not, defaults to the latest on this channel.
-        beamPackages = pkgs.beam.packagesWith pkgs.beam.interpreters.erlang_27;
-        # Declare the Elixir version you want to use. If not, defaults to the latest on this channel.
-        elixir = beamPackages.elixir_1_18;
-        # Import a development shell we'll declare in `shell.nix`.
-        devShell = import ./shell.nix { inherit pkgs beamPackages; };
+  outputs = { self, nixpkgs }: let
+    overlay = prev: final: rec {
+      beamPackages = prev.beam.packagesWith prev.beam.interpreters.erlang_27;
+      elixir = beamPackages.elixir_1_17;
+      erlang = prev.erlang_27;
+      hex = beamPackages.hex;
+      final.mix2nix = prev.mix2nix.overrideAttrs {
+        nativeBuildInputs = [ final.elixir ];
+        buildInputs = [ final.erlang ];
+      };
+    };
 
-        my-elixir-app = let
-            lib = pkgs.lib;
-            # FIXME: Import the Mix deps into Nix by running
-            # mix2nix > nix/deps.nix
-            # mixNixDeps = import ./deps.nix {inherit lib beamPackages;};
-          in beamPackages.mixRelease {
-            pname = "my-elixir-app";
-            # Elixir app source path
+    forAllSystems = nixpkgs.lib.genAttrs [
+      "x86_64-linux"
+      "aarch64-linux"
+      "x86_64-darwin"
+      "aarch64-darwin"
+    ];
+
+    nixpkgsFor = system:
+      import nixpkgs {
+        inherit system;
+        overlays = [overlay];
+      };
+    in {
+    packages = forAllSystems(system: let
+      pkgs = nixpkgsFor system;
+      # FIXME: import the Mix deps into Nix by running `mix2nix > deps.nix` from a dev shell
+      #mixNixDeps = import ./deps.nix {
+      #  lib = pkgs.lib;
+      #  beamPackages = pkgs.beamPackages;
+      #};
+      in rec {
+        default = pkgs.beamPackages.mixRelease {
+            pname = "my-elixir-lib";
+            # Elixir lib source path
             src = ./.;
             version = "0.1.0";
 
-            # FIXME: mixNixDeps was specified in the FIXME above. Uncomment the next line.
+	    # FIXME: once you've addressed the fixme comment above,
+	    # uncomment the following line to include mixNixDeps
             # inherit mixNixDeps;
 
-            # Add other inputs to the build if you need to
-            buildInputs = [ elixir ];
+	    # Add inputs to the build if you need to
+            buildInputs = [ pkgs.elixir ];
           };
-      in
-      {
-        devShells.default = devShell;
-        packages.default = my-elixir-app;
-      }
-    );
+      });
+
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor system;
+    in {
+      default = pkgs.callPackage ./shell.nix {};
+    });
+  };
 }
 
